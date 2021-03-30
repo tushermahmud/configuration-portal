b61903aafa1666c2166d0b957c682453bb03f31b
tushermahmud <sazzadmahmud01795355849@gmail.com>
2021-03-31 02:37:53 +0600

    the app background is changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,26 @@
-import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
-import { Platform } from '@angular/cdk/platform';
-import { TranslateService } from '@ngx-translate/core';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { Component, Inject, OnDestroy, OnInit } from "@angular/core";
+import { DOCUMENT } from "@angular/common";
+import { Platform } from "@angular/cdk/platform";
+import { TranslateService } from "@ngx-translate/core";
+import { Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
 
-import { FuseConfigService } from '@fuse/services/config.service';
-import { FuseNavigationService } from '@fuse/components/navigation/navigation.service';
-import { FuseSidebarService } from '@fuse/components/sidebar/sidebar.service';
-import { FuseSplashScreenService } from '@fuse/services/splash-screen.service';
-import { FuseTranslationLoaderService } from '@fuse/services/translation-loader.service';
+import { FuseConfigService } from "@fuse/services/config.service";
+import { FuseNavigationService } from "@fuse/components/navigation/navigation.service";
+import { FuseSidebarService } from "@fuse/components/sidebar/sidebar.service";
+import { FuseSplashScreenService } from "@fuse/services/splash-screen.service";
+import { FuseTranslationLoaderService } from "@fuse/services/translation-loader.service";
 
-import { navigation } from 'app/navigation/navigation';
-import { locale as navigationEnglish } from 'app/navigation/i18n/en';
-import { locale as navigationTurkish } from 'app/navigation/i18n/tr';
+import { navigation } from "app/navigation/navigation";
+import { locale as navigationEnglish } from "app/navigation/i18n/en";
+import { locale as navigationTurkish } from "app/navigation/i18n/tr";
 
 @Component({
-    selector   : 'app',
-    templateUrl: './app.component.html',
-    styleUrls  : ['./app.component.scss']
+    selector: "app",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.scss"],
 })
-export class AppComponent implements OnInit, OnDestroy
-{
+export class AppComponent implements OnInit, OnDestroy {
     fuseConfig: any;
     navigation: any;
 
@@ -49,28 +48,30 @@ export class AppComponent implements OnInit, OnDestroy
         private _fuseTranslationLoaderService: FuseTranslationLoaderService,
         private _translateService: TranslateService,
         private _platform: Platform
-    )
-    {
+    ) {
         // Get default navigation
         this.navigation = navigation;
 
         // Register the navigation to the service
-        this._fuseNavigationService.register('main', this.navigation);
+        this._fuseNavigationService.register("main", this.navigation);
 
         // Set the main navigation as our current navigation
-        this._fuseNavigationService.setCurrentNavigation('main');
+        this._fuseNavigationService.setCurrentNavigation("main");
 
         // Add languages
-        this._translateService.addLangs(['en', 'tr']);
+        this._translateService.addLangs(["en", "tr"]);
 
         // Set the default language
-        this._translateService.setDefaultLang('en');
+        this._translateService.setDefaultLang("en");
 
         // Set the navigation translations
-        this._fuseTranslationLoaderService.loadTranslations(navigationEnglish, navigationTurkish);
+        this._fuseTranslationLoaderService.loadTranslations(
+            navigationEnglish,
+            navigationTurkish
+        );
 
         // Use a language
-        this._translateService.use('en');
+        this._translateService.use("en");
 
         /**
          * ----------------------------------------------------------------------------------------------------
@@ -106,9 +107,8 @@ export class AppComponent implements OnInit, OnDestroy
          */
 
         // Add is-mobile class to the body if the platform is mobile
-        if ( this._platform.ANDROID || this._platform.IOS )
-        {
-            this.document.body.classList.add('is-mobile');
+        if (this._platform.ANDROID || this._platform.IOS) {
+            this.document.body.classList.add("is-mobile");
         }
 
         // Set the private defaults
@@ -122,36 +122,30 @@ export class AppComponent implements OnInit, OnDestroy
     /**
      * On init
      */
-    ngOnInit(): void
-    {
+    ngOnInit(): void {
         // Subscribe to config changes
         this._fuseConfigService.config
             .pipe(takeUntil(this._unsubscribeAll))
             .subscribe((config) => {
-
                 this.fuseConfig = config;
 
                 // Boxed
-                if ( this.fuseConfig.layout.width === 'boxed' )
-                {
-                    this.document.body.classList.add('boxed');
-                }
-                else
-                {
-                    this.document.body.classList.remove('boxed');
+                if (this.fuseConfig.layout.width === "boxed") {
+                    this.document.body.classList.add("boxed");
+                } else {
+                    this.document.body.classList.remove("boxed");
                 }
 
                 // Color theme - Use normal for loop for IE11 compatibility
-                for ( let i = 0; i < this.document.body.classList.length; i++ )
-                {
+                for (let i = 0; i < this.document.body.classList.length; i++) {
                     const className = this.document.body.classList[i];
 
-                    if ( className.startsWith('theme-') )
-                    {
+                    if (className.startsWith("theme-")) {
                         this.document.body.classList.remove(className);
                     }
                 }
-
+                console.log(this.fuseConfig.colorTheme);
+                console.log(this.document.body.classList);
                 this.document.body.classList.add(this.fuseConfig.colorTheme);
             });
     }
@@ -159,8 +153,7 @@ export class AppComponent implements OnInit, OnDestroy
     /**
      * On destroy
      */
-    ngOnDestroy(): void
-    {
+    ngOnDestroy(): void {
         // Unsubscribe from all subscriptions
         this._unsubscribeAll.next();
         this._unsubscribeAll.complete();
@@ -175,8 +168,7 @@ export class AppComponent implements OnInit, OnDestroy
      *
      * @param key
      */
-    toggleSidebarOpen(key): void
-    {
+    toggleSidebarOpen(key): void {
         this._fuseSidebarService.getSidebar(key).toggleOpen();
     }
 }
